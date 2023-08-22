779023ef156610f2d7967293fc93a0352c834eba
topu12345 <topu15-3604@diu.edu.bd>
2023-08-22 17:18:17 +0600

    login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,11 @@
 import userModel from "../models/userModel.js";
-import {hashPassword} from './../helpers/authHelper.js'
+import {comparepassword, hashPassword} from './../helpers/authHelper.js'
+import JWT from 'jsonwebtoken';
 
 export const registerController =async(req,res)=>{
      
      try{
-       const {name,email,password, phone,addres}=req.body
+       const {name,email,password, phone,address}=req.body
        // validation
        if(!name){
         return res.send({error:'Name is Required'}) 
@@ -18,13 +19,13 @@ export const registerController =async(req,res)=>{
        if(!phone){
         return res.send({error:'phone Number is Required'}) 
        }
-       if(!addres){
+       if(!address){
         return res.send({error:'Addres is Required'}) 
        }
 
 
        // check user
-       const exisitingUser =await userModal.findOne({email})
+       const exisitingUser =await userModel.findOne({email})
        // exisiting user
        if(exisitingUser){
         return res.status(200).send({
@@ -35,7 +36,7 @@ export const registerController =async(req,res)=>{
       // resgister user
       const hashedPassword=await hashPassword(password)
       //save
-      const user = new userModel({name,email, phone,addres,password:hashedPassword}).save()
+      const user = await new userModel({name,email, phone,address,password:hashedPassword}).save()
       res.status(201).send({
         success:true,
         message: "user Register Successfully",
@@ -51,5 +52,57 @@ export const registerController =async(req,res)=>{
         messahe:'error in registeration'
       })
      }
+};
+
+//POST LOGIN
+
+export const loginController=async(req,res)=>{
+try{
+const {email,password}=req.body;
+//validation
+if(!email || !password){
+  return res.status(404).send({
+    success:false,
+    message:'Invalid email or password'
+  })
+}
+//cheack user
+const user = await userModel.findOne({ email });
+if (!user) {
+  return res.status(404).send({
+    success: false,
+    message: "Email is not registerd",
+  });
+}
+const  match = await comparepassword(password,user.password)
+if(!match){
+  return res.status(200).send({
+    success:false,
+    message:'Invaild password'
+  })
+}
+//tokan
+const token =await JWT.sign({_id:user._id}, process.env.JWT_SECRET,{expiresIn:"7d",});
+res.status(200).send({
+  success: true,
+  message:'login successfully',
+  user:{
+    name: user.name,
+    email:user.email,
+    phone:user.phone,
+    address:user.address,
+    role: user.role,
+  },
+  token,
+});
+}
+catch(error){
+console.log(error)
+res.status(500).send({
+  success:false,
+  message:'error in login',
+  error
+})
+}
 };
 
--- models/userModel.js
+++ models/userModel.js
@@ -20,7 +20,7 @@ const userSchema =new mongoose.Schema({
         type:String,
         required:true
     },
-    addres:{
+    address:{
         type:String,
         required:true 
     },

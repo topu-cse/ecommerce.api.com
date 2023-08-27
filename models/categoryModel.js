8acfedc2e62e1f58525fc7fe48d1ec9624337678
topu12345 <topu15-3604@diu.edu.bd>
2023-08-28 00:15:41 +0600

    create product

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -4,12 +4,12 @@ import mongoose from "mongoose";
 const categorySchema =new mongoose.Schema({
     name:{
         type:String,
-        require:true,
-        unique:true,
+        //require:true,
+        //unique:true,
     },
     slug:{
         type:String,
         lowercade:true,
     },
 })
-export default mongoose.model("category",categorySchema);
+export default mongoose.model("Category",categorySchema);
--- routes/productRouters.js
+++ routes/productRouters.js
@@ -1,11 +1,29 @@
 import express from 'express'
 import { isAdmin, requireSignIn } from '../middlewares/authMiddleware.js'
-import { createProductController } from '../controllers/productController.js'
+import { createProductController,
+     deleteProductController,
+     getProductController, 
+     getSingleProductController, 
+     productPhotoController, 
+     updateProductController} 
+     from '../controllers/productController.js'
 import formidable from 'express-formidable'
 const router=express.Router()
 
 //router
 router.post('/create-product',requireSignIn,isAdmin, formidable(),createProductController)
 
+//get product
+router.get('/get-product',getProductController)
+
+//single product
+router.get('/get-product/:slug',getSingleProductController)
+
+//get photo
+router.get("/product-photo/:pid", productPhotoController);
+//delete rproduct
+router.delete("/delete-product/:pid", deleteProductController);
+//update-product
+router.put('/update-product/:pid',requireSignIn,isAdmin, formidable(),updateProductController)
 
 export default router
